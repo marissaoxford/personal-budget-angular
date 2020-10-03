bf85ae38ef722da21f630822664f998f426852a5
Delijume <moxford3@uncc.edu>
2020-10-02 23:14:00 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -5,23 +5,7 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    },
-    ]
-};
-
+const budget = require("./budget");
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
